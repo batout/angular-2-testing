95adffd04698bd2f58f235a922731a52bffbbed2
batout <amz93@outlook.com>
2017-06-28 05:27:52 +0200

    added firebase, realtime list

--- a2website/src/app/components/pages/home/home.component.ts
+++ a2website/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable,FirebaseObjectObservable } from 'angularfire2/database';
 
 
 
@@ -10,10 +10,31 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  items: FirebaseListObservable<any[]>;
+  item: FirebaseObjectObservable<any>;
+  items:FirebaseListObservable<any[]>;
+  values:any[]=[];
   constructor( private db :AngularFireDatabase) {
-        this.items = db.list('/items');
+        this.item = db.object('/items');
+        this.items = db.list('items',{ preserveSnapshot: true });
 
+        this.items
+        .subscribe(snapshots => {
+          if(this.values !=[]){
+            this.values=[];
+          }
+          snapshots.forEach((snapshot) => {
+            console.log(snapshot.key)
+            console.log(snapshot.val())
+             this.values.push(snapshot.val());
+              });
+            })
+
+
+
+   }
+
+   getValues(){
+     console.log(this.values);
    }
 
   ngOnInit() {
